0fd486e61d9d7db34733e4bc8aca0e743bca87cd
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-04-17 12:03:06 +0200

    change

---
+++ checkpoint7.js
@@ -0,0 +1,12 @@
+function math(arg1, arg2, arg3, arg4) {
+    let calculo = (arg1 + arg2) * (arg3 + arg4);
+    if (calculo > 50) {
+      console.log("¡El número es mayor que 50!");
+    } else if (calculo = 50) {
+      console.log("¡Tu número es 50!")
+    } else {
+      console.log("¡El número es menor que 50!");
+    }
+  }
+  
+  console.log(math(9, 5, 3, 7));
